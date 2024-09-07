173e3385ae69d415aa0f82494c9d64a178e60c24
wuweihua <175990271@qq.com>
2024-09-07 14:17:48 +0800

    3213123

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), ViteAliases()],
 	build: {
-		outDir: 'C:\\Users\\wuweihua\\codeProject\\MaterialEditPython5.0\\static',
+		outDir: 'C:\\Users\\aimlo\\codeProject\\MaterialEditPython50\\static',
 		emptyOutDir: true,
 	},
 	base: './',
 });
-
